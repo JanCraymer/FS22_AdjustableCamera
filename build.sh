928b842fc4cd0e2a329fa8e16b24d43559e2cadd
Jan <26329438+JanCraymer@users.noreply.github.com>
2024-02-23 19:26:04 +0100

    update build script

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 ZIPFILE=FS22_AdjustableCamera.zip
 if test -f "$ZIPFILE"; then
     echo "Deleting previous $ZIPFILE."
-    rm FS22_AdjustableCamera.zip
+    rm $ZIPFILE
 fi
 
 echo "Creating new $ZIPFILE."
-zip FS22_AdjustableCamera.zip modDesc.xml icon.dds *.lua > /dev/null
+zip $ZIPFILE modDesc.xml icon.dds *.lua > /dev/null
 
 if unzip -t "$ZIPFILE" > /dev/null; then
     echo "Zip file integrity check successful."
